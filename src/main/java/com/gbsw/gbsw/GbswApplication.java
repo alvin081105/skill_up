434c1387bbf8d032ff86ba5b16cefd8f7649402e
alvin081105 <ca081105@gmail.com>
2025-07-07 12:28:59 +0900

    포트 설정

---
+++ src/main/java/com/gbsw/gbsw/GbswApplication.java
@@ -0,0 +1,13 @@
+package com.gbsw.gbsw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbswApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GbswApplication.class, args);
+	}
+
+}
