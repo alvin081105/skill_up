7d8ec676e3a5ba2c491adaa0287bbdfa6c470297
alvin081105 <ca081105@gmail.com>
2025-07-07 12:28:48 +0900

    spring boot생성

---
+++ src/test/java/com/gbsw/gbsw/GbswApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gbsw.gbsw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GbswApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
