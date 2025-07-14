68cb1e2b20e53f32f01a1c3918735e3fdfb4ec2e
alvin081105 <ca081105@gmail.com>
2025-07-14 14:21:53 +0900

    신고 respository

---
+++ src/main/java/com/gbsw/gbsw/repository/ReportRepository.java
@@ -0,0 +1,7 @@
+package com.gbsw.gbsw.repository;
+
+import com.gbsw.gbsw.entity.Report;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReportRepository extends JpaRepository<Report, Long> {
+}
